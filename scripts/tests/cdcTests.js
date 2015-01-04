813b414085128bd65e208dfa22bede9f208c8be8
Yusuke Matsubara <whym@whym.org>
2015-01-04 19:49:31 +0900

    QUnit unittests

---
+++ scripts/tests/cdcTests.js
@@ -0,0 +1,22 @@
+"use strict";
+require.config({
+	baseUrl: 'scripts',
+	paths: {
+		cdc: 'cdc'
+	}
+});
+define(
+	['cdc'],
+	function(cdc) {
+		var run = function() {
+			test('genDates: 2 dates with a 2-day interval', function() {
+				deepEqual(cdc.genDates(new Date(Date.UTC(2014, 3, 3)), 2, 2),
+					  [
+						  "2014-04-01T23:59:59",
+						  "2014-04-03T23:59:59",
+					  ]);
+			});
+		};
+		return {run: run};
+	}
+);
---
+++ scripts/unittests.js
@@ -0,0 +1,37 @@
+"use strict";
+require.config({
+	baseUrl: 'scripts',
+	paths: {
+		'QUnit': 'http://code.jquery.com/qunit/qunit-1.16.0',
+		'cdcTests': 'tests/cdcTests'
+	},
+	shim: {
+	   'QUnit': {
+		   exports: 'QUnit',
+		   init: function() {
+			   QUnit.config.autoload = false;
+			   QUnit.config.autostart = false;
+		   }
+	   } 
+	}
+});
+(function(url){
+	var link = document.createElement('link');
+	link.type = 'text/css';
+	link.rel = 'stylesheet';
+	link.href = url;
+	document.getElementsByTagName('head')[0].appendChild(link);
+})('http://code.jquery.com/qunit/qunit-1.16.0.css');
+
+
+// require the unit tests.
+require(
+	['QUnit', 'cdcTests'],
+	function(QUnit, cdcTests) {
+		// run the tests.
+		cdcTests.run();
+		// start QUnit.
+		QUnit.load();
+		QUnit.start();
+	}
+);
